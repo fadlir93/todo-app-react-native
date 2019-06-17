4e03698ed853e413499115139062ae27f1509c1c
Fadli Ramadhan <fadlir93@gmail.com>
2019-06-17 08:42:58 +0700

    First Commit

---
+++ app/components/Main.js
@@ -0,0 +1,34 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  StyleSheet, 
+  Text, 
+  View,
+  TextInput,
+  ScrollView,
+  TouchableOpacity
+} from 'react-native';
+
+
+// type Props = {};
+export default class Main extends React.Component {
+  render() {
+    return (
+      <View><Text>Testing</Text></View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+
+  },
+});
+
